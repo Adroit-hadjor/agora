add218d1e3826193cbf4570a011fbc07e62af3b9
Ofoe Hadjor <ofoehadjor@gmail.com>
2021-03-25 11:54:19 +0000

    fdf

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import Index from './pages/index'
+import Main from './pages/main'
+import Upload from './pages/upload'
+import Meeting from './pages/meeting'
+import { BrowserRouterHook } from './utils/use-router'
+function App () {
+  return (
+    <BrowserRouterHook>
+      <Switch>
+      <Route exact path="/upload" component={Upload}></Route>
+        <Route exact path="/meeting/:name" component={Meeting}></Route>
+        <Route  path="/pastor" component={Index}></Route>
+        <Route path="/" component={Main}></Route>
+       
+      </Switch>
+    </BrowserRouterHook>
+  )
+}
+
+export default App
